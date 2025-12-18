4394b3f7b70138715d35f98376656876c6ec8d9c
csrirammganesh <sriramearning23@gmail.com>
2025-12-18 15:04:14 +0530

    Create Main.java

---
+++ java-app/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java Level-2 container!");
+        System.out.println("Port 8080 is exposed (container level) for future use.");
+
+        // Keep the container alive so we can test Docker port mapping
+        try {
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
